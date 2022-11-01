132b31f3b77a1d080f821d04770932faa07e441b
Lieyanto Sim <99237213+Lieyanto01@users.noreply.github.com>
2022-11-01 20:18:19 +0700

    Menambahkan Komponen Halaman
    Menambahkan judul, sidebar, tombol upload file, dan menampilkan gambar

--- pages/01_💻_Application.py
+++ pages/01_💻_Application.py
@@ -1,5 +1,29 @@
+#Import Library
+from turtle import width
 from api import logic, utils
 import streamlit as st
+import numpy as np
+from PIL import Image
+
+#Membuat Judul halaman
+st.markdown(""" <style> .font {font-size:35px ; font-family: 'Cooper Black'; color: #FF9633;} 
+</style> """, unsafe_allow_html=True)
+st.markdown('<p class="font" style="text-align: center;">Upload Image here...</p>', unsafe_allow_html=True)
+
+#Membuat Sidebar
+st.sidebar.markdown('<p class="font">My First Photo Converter App</p>', unsafe_allow_html=True)
+with st.sidebar.expander("About the App"):
+     st.write("""
+    Aplikasi ini dibuat untuk mereduksi Speckle Noise pada citra.  \n  \nDibuat oleh tim US GE. Hope you enjoy!
+     """)
+
+#Memunculkan tombol file upload
+file_uploader = st.file_uploader("", type=['jpg', 'png', 'jpeg'])
+
+if file_uploader is not None:
+    image = Image.open(file_uploader)
+    #st.markdown('<p style="text-align: center;">Before</p>',unsafe_allow_html=True)
+    st.image(image, width=300)
 
 
 def main():
