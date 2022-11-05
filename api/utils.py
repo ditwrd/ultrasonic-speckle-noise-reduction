08cb4182b8fcc24c874a732dd6c006296c511fad
Aditya Wardianto <aditya.wardianto@zero-one-group.com>
2022-11-05 14:24:24 +0700

    feat: added open image utility

---
+++ api/utils.py
@@ -0,0 +1,15 @@
+import numpy as np
+from PIL import Image, ImageOps
+
+
+def open_image(image_buffer):
+    """
+        image_buffer : The image buffer from streamlit st.upload_file API
+
+    Returns:
+       A 2D numpy array containing the grayscaled image
+    """
+    rgba_image = Image.open(image_buffer)
+    grayscale_image = ImageOps.grayscale(rgba_image)
+    grayscale_image_array = np.asarray(grayscale_image)
+    return grayscale_image_array
