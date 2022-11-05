2832f75faa1a13788a4bf495bb31f3cbb30bcb7d
Aditya Wardianto <aditya.wardianto@zero-one-group.com>
2022-11-05 12:45:00 +0700

    chore: reformat code

--- pages/01_💻_Application.py
+++ pages/01_💻_Application.py
@@ -1,28 +1,40 @@
-#Import Library
+# Import Library
 from turtle import width
-from api import logic, utils
-import streamlit as st
+
 import numpy as np
+import streamlit as st
 from PIL import Image
 
-#Membuat Judul halaman
-st.markdown(""" <style> .font {font-size:35px ; font-family: 'Cooper Black'; color: #FF9633;} 
-</style> """, unsafe_allow_html=True)
-st.markdown('<p class="font" style="text-align: center;">Upload Image here...</p>', unsafe_allow_html=True)
+from api import logic, utils
 
-#Membuat Sidebar
-st.sidebar.markdown('<p class="font">My First Photo Converter App</p>', unsafe_allow_html=True)
+# Membuat Judul halaman
+st.markdown(
+    """ <style> .font {font-size:35px ; font-family: 'Cooper Black'; color: #FF9633;} 
+</style> """,
+    unsafe_allow_html=True,
+)
+st.markdown(
+    '<p class="font" style="text-align: center;">Upload Image here...</p>',
+    unsafe_allow_html=True,
+)
+
+# Membuat Sidebar
+st.sidebar.markdown(
+    '<p class="font">My First Photo Converter App</p>', unsafe_allow_html=True
+)
 with st.sidebar.expander("About the App"):
-     st.write("""
+    st.write(
+        """
     Aplikasi ini dibuat untuk mereduksi Speckle Noise pada citra.  \n  \nDibuat oleh tim US GE. Hope you enjoy!
-     """)
+     """
+    )
 
-#Memunculkan tombol file upload
-file_uploader = st.file_uploader("", type=['jpg', 'png', 'jpeg'])
+# Memunculkan tombol file upload
+file_uploader = st.file_uploader("", type=["jpg", "png", "jpeg"])
 
 if file_uploader is not None:
     image = Image.open(file_uploader)
-    #st.markdown('<p style="text-align: center;">Before</p>',unsafe_allow_html=True)
+    # st.markdown('<p style="text-align: center;">Before</p>',unsafe_allow_html=True)
     st.image(image, width=300)
 
 
