b7e17dffaa42bd9ef360f0d855f856d6ad1d00cd
Aditya Wardianto <aditya.wardianto@zero-one-group.com>
2022-11-07 17:49:17 +0700

    fix: fix bugs (alot of them)

--- api/logic.py
+++ api/logic.py
@@ -1,16 +1,17 @@
 import numpy as np
 import numpy.typing as npt
+from stqdm import stqdm
 
 
 def _normal_gaussian_fuzzy_membership(
     std: int, a: npt.NDArray[np.int], b: float
 ) -> npt.NDArray[np.float_]:
 
-    precomputed_sigma: np.int = 2 * np.square(std)
+    precomputed_sigma: np.int = 2 * np.square(std) + 10e-5
 
     # Check whether sigma is 0
-    if precomputed_sigma == 0:
-        raise Exception("Computed sigma/standard deviation is 0")
+    # if precomputed_sigma == 0:
+    #     raise Exception("Computed sigma/standard deviation is 0")
 
     divider: np.float = 1 / np.sqrt(precomputed_sigma * np.pi)
     exponential: npt.NDArray[np.float_] = np.exp(-np.square(a - b) / precomputed_sigma)
@@ -22,10 +23,12 @@ def _window_membership(image_window: npt.NDArray[np.int_]) -> npt.ArrayLike:
     i_avg: np.float_ = np.mean(image_window)
     std: np.float_ = np.std(image_window)
 
+    # image_window = np.where(image_window < i_avg, 0, image_window)
+
     # Calculate fuzzy membership
     membership = _normal_gaussian_fuzzy_membership(std, image_window, i_avg)
     # Get the 2D coordinate of the max value
-    row_max, col_max = np.where(membership == membership.max())
+    row_max, col_max = np.unravel_index(membership.argmax(), membership.shape)
     # Translate the coord from membership to the image window
     image_val_max_member = image_window[row_max, col_max]
 
@@ -40,13 +43,12 @@ def gaussian_fuzzy_filter(
         raise Exception("Window width need to be odd")
 
     padded_image: npt.NDArray[np.int_] = np.pad(image_array, (width - 1) // 2)
-
+    image_array_processed = np.copy(image_array)
     n_row, n_col = image_array.shape
     # For loop is unoptimizable due to the fact that the process seems to be  sequential
-    for i_row in range(n_row):
+    for i_row in stqdm(range(n_row)):
         for i_col in range(n_col):
-            image_array[i_row, i_col] = _window_membership(
+            image_array_processed[i_row, i_col] = _window_membership(
                 padded_image[i_row : i_row + width, i_col : i_col + width]
             )
-
-    return image_array
+    return image_array_processed
