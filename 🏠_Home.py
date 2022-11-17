8c85b5266802cdd46c07a24c32ce0af33560854f
Lieyanto Sim <99237213+Lieyanto01@users.noreply.github.com>
2022-11-17 10:09:45 +0700

    Menambahkan Bagian Home Page
    Menambahkan Judul, Nama anggota

--- 🏠_Home.py
+++ 🏠_Home.py
@@ -2,9 +2,18 @@ import streamlit as st
 
 
 def main():
-    st.header("Assignment Name")
-    st.subheader("Class Name")
-    st.markdown("Aditya Wardianto 07311940000001")
+    st.header("Speckle Noise Reduction from Medical Images Using Gaussian Fuzzy Membership Function")
+    st.subheader("Ultrasound GE Team")
+    st.markdown("""
+    - Chandra kartika 07311840000039
+    - Aditya Wardianto 07311940000001
+    - Lieyanto Sim 07311940000029
+    - Aghnia Hasya Affan 07311940000059
+    - Tasya Aulia Fahria 07311940000065
+    - Angeline Dwi Sanjaya 5023201017
+    - Nadhira Anindyafitri Prakosa 5023201052
+    - Ariq Dreiki Hajjanto 5023201057
+    """)
     st.markdown("Made with ❤️, 🦾,  and ☕")
 
 if __name__ == "__main__":
