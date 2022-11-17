aff9d85ac84c0174cb02ee4f61eba7b4422257f1
Aditya Wardianto <aditya.wardianto@zero-one-group.com>
2022-11-17 10:13:48 +0700

    chores: home page config

--- 🏠_Home.py
+++ 🏠_Home.py
@@ -2,9 +2,12 @@ import streamlit as st
 
 
 def main():
-    st.header("Speckle Noise Reduction from Medical Images Using Gaussian Fuzzy Membership Function")
+    st.header(
+        "Speckle Noise Reduction from Medical Images Using Gaussian Fuzzy Membership Function"
+    )
     st.subheader("Ultrasound GE Team")
-    st.markdown("""
+    st.markdown(
+        """
     - Chandra kartika 07311840000039
     - Aditya Wardianto 07311940000001
     - Lieyanto Sim 07311940000029
@@ -13,8 +16,11 @@ def main():
     - Angeline Dwi Sanjaya 5023201017
     - Nadhira Anindyafitri Prakosa 5023201052
     - Ariq Dreiki Hajjanto 5023201057
-    """)
+    """
+    )
     st.markdown("Made with ❤️, 🦾,  and ☕")
 
+
 if __name__ == "__main__":
+    st.set_page_config(page_title="Home", page_icon="🏠")
     main()
